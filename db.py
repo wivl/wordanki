490a671dec427349e695a22ecc33823a0d9d01f4
wivl <68458084+wivl@users.noreply.github.com>
2023-03-17 17:33:51 +0800

    Add features

--- db.py
+++ db.py
@@ -1,9 +1,33 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2023 wivl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from tinydb import TinyDB, Query
 import json
 
 dictionary = TinyDB('./db/dict.json')
 
-filenames = ['./dict/KaoYan_1.json', './dict/KaoYan_2.json', './dict/KaoYan_3.json']
+filenames = ['./dict/KaoYan_2.json', './dict/CET4luan_2.json', './dict/CET6_2.json', './dict/Level8_2.json']
 
 
 if __name__ == '__main__':
@@ -12,6 +36,9 @@ if __name__ == '__main__':
         for line in file.readlines():
             words = line.strip()
             word_json = json.loads(words)
+            Word = Query()
+            if len(dictionary.search(Word.headWord == word_json['headWord'])) != 0:
+                continue
             dictionary.insert(word_json)
     
         file.close()
--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2023 wivl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import genanki
 from tinydb import TinyDB, Query
 
@@ -7,6 +31,16 @@ listdb = TinyDB('./db/words.json')
 
 dictdb = TinyDB('./db/dict.json')
 
+CSS = """.card {
+ font-family: arial;
+ font-size: 20px;
+ text-align: center;
+ color: black;
+ background-color: white;
+}
+"""
+
+# Card model
 WordCard = genanki.Model(
         1276008402,
         "Card Model",
@@ -19,25 +53,30 @@ WordCard = genanki.Model(
         templates=[
             {
                 'name': 'Card 1',
-                'qfmt': '{{Word}}<br>{{SentenceEn}}',
-                'afmt': '{{FrontSide}}<br>{{SentenceZh}}<br id="answer">{{Meaning}}'
+                'qfmt': '<br><br>{{Word}}<br><br><br>{{SentenceEn}}',
+                'afmt': '{{FrontSide}}<br><br>{{SentenceZh}}<br><br><br id="answer">{{Meaning}}'
                 },
-            ])
+            ],
+        css=CSS
+        )
 
+# deck model
 WordDeck = genanki.Deck(
     2054490786,
     'Words')
 
 
+# Add word to listdb
 def new_word(word):
     Word = Query()
     if len(listdb.search(Word.word == word)) == 0:
         listdb.insert({'word': word})
-        print('Word ' + word + ' inserted' + ', ' + str(len(listdb.all())) + ' words in total.')
+        print('Word "' + word + '" inserted' + ', ' + str(len(listdb.all())) + ' words in total.')
     else:
-        print('Word ' + word + ' already exists, skipping.')
+        print('Word "' + word + '" already exists, skipping.')
 
 
+# Go through listdb and generate a list of NOTES
 def make_notes():
     notes = []
     words = listdb.all()
@@ -45,19 +84,25 @@ def make_notes():
     if len(words) != 0:
         for word in words:
             item = dictdb.search(Dict.headWord == word['word'])
-            print(len(item))
             if len(item) == 0:
-                print('Word ' + word['word'] + ' is not in current dictionary, skipping.')
-                exit(1)
+                print('Word "' + word['word'] + '" is not in current dictionary, skipping.')
+                continue
+            
+            print('Adding word "' + word['word'] + '"...')
 
 
             content = []
             # 单词
             content.append(item[0]['headWord'])
             # 英句
-            content.append(item[0]['content']['word']['content']['sentence']['sentences'][0]['sContent'])
+            if 'sentence' in item[0]['content']['word']['content'].keys():
+
+                content.append(item[0]['content']['word']['content']['sentence']['sentences'][0]['sContent'])
             # 中句
-            content.append(item[0]['content']['word']['content']['sentence']['sentences'][0]['sCn'])
+                content.append(item[0]['content']['word']['content']['sentence']['sentences'][0]['sCn'])
+            else:
+                content.append('<br>')
+                content.append('<br>')
             # 词性
             # 意思
             meaning_str = ''
@@ -79,6 +124,7 @@ def make_notes():
     return notes
     
 
+# Using list of NOTES to generate deck and save
 def gen_deck(deck, notes):
     if len(notes) == 0:
         print('Notes list is empty, skipping.')
@@ -87,28 +133,51 @@ def gen_deck(deck, notes):
             deck.add_note(note)
         genanki.Package(deck).write_to_file('output.apkg')
 
+def print_help():
+    print('Usage: python3 main.py [opt] <arg>')
+    print('\tOptions:')
+    print('\t\t-h print this help')
+    print('\t\t-a <word> required, add a word to list')
+    print('\t\t-g generate word deck using current list')
+    print('\t\t-r <filepath> required, read text file and write them to listdb')
 
 
 
 def main(argv):
+    if len(sys.argv) <= 1:
+        print('len < 1')
+        print_help()
+        sys.exit(1)
     try:
-        opts, args = getopt.getopt(argv,"ha:g")
+        opts, args = getopt.getopt(argv,"hga:r:")
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
+        print_help()
+        sys.exit(1)
 
     for opt, arg in opts:
         if opt == '-h':
-            print('TODO, help')
+            print_help()
             sys.exit()
+
         elif opt == '-a':
             new_word(arg)
+            sys.exit()
+
         elif opt == '-g':
-            print('TODO, generate anki deck')
             notes = make_notes()
             gen_deck(WordDeck, notes)
+            sys.exit()
+
+        elif opt == '-r':
+            filename = arg
+            with open(filename) as fin:
+                for line in fin.readlines():
+                    word = line.strip()
+                    new_word(word)
+                    
         else:
-            print('TODO, unknown opt and print help')
+            print('[ERROR] Unknown option "' + opt + '"')
+            print_help()
             
 
 if __name__ == '__main__':
